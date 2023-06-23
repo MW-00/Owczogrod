515748abd232aef39ac4aeea17643b988ac796c7
MW-00 <86376368+MW-00@users.noreply.github.com>
2023-06-23 09:57:42 +0200

    Initial commit for client comments

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// $(document).ready(function () {
+
+//     $(".czytaj-wiecej").click(function () {
+//         $(this).closest(".elementOferty").find(".hidden").fadeToggle();
+//         $(this).closest(".elementOferty").find(".hidden").css("display", "flex");
+//     })
+
+// })
